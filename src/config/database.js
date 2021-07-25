46b4ef8f7c8a7699aaf1ea973ea1df14c114a271
KitakatsuTed <ted@circlearound.co.jp>
2021-07-25 12:18:12 +0900

    fix

--- src/config/database.js
+++ src/config/database.js
@@ -23,12 +23,7 @@ module.exports = {
     "logQueryParameters" : true
   },
   "production": {
-    // 'username': process.env.DB_USERNAME || 'postgres',
-    // 'password': process.env.DB_PASS || 'password',
     'database': process.env.DB_DATABASE || "express_production",
-    // 'host': process.env.DB_HOST || "127.0.0.1",
-    // 'port': process.env.DB_PORT || 5432,
-
     "dialect": process.env.DB_DIALECT || "postgres",
     // "benchmark": true,
     "logQueryParameters" : true,
