3b468f61d15b7267eb259437946a0c4b9ecf465c
KitakatsuTed <ted@circlearound.co.jp>
2021-07-25 10:53:10 +0900

    fix

--- src/config/database.js
+++ src/config/database.js
@@ -32,7 +32,7 @@ module.exports = {
     "dialect": process.env.DB_DIALECT || "postgres",
     // "benchmark": true,
     "logQueryParameters" : true,
-    "dialectOptions": { ssl: true }
+    "dialectOptions": { ssl: true, rejectUnauthorized: false }
   }
 };
 
