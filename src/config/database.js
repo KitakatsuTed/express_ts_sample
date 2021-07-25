1fe5d7edfd0a0b4821374fb2f0dc36dce39d633b
KitakatsuTed <ted@circlearound.co.jp>
2021-07-25 12:09:44 +0900

    fix

--- src/config/database.js
+++ src/config/database.js
@@ -2,26 +2,6 @@
 require('dotenv').config();
 
 module.exports = {
-  "development": {
-    'username': process.env.DB_USERNAME || 'postgres',
-    'password': process.env.DB_PASS || 'password',
-    'database': process.env.DB_DATABASE || "express_development",
-    'host': process.env.DB_HOST || "127.0.0.1",
-    'port': process.env.DB_PORT || 5432,
-    "dialect": process.env.DB_DIALECT || "postgres",
-    "benchmark": true,
-    "logQueryParameters" : true
-  },
-  "test": {
-    'username': process.env.DB_USERNAME || 'postgres',
-    'password': process.env.DB_PASS || 'password',
-    'database': process.env.DB_DATABASE || "express_test",
-    'host': process.env.DB_HOST || "127.0.0.1",
-    'port': process.env.DB_PORT || 5432,
-    "dialect": process.env.DB_DIALECT || "postgres",
-    "benchmark": true,
-    "logQueryParameters" : true
-  },
   "production": {
     // 'username': process.env.DB_USERNAME || 'postgres',
     // 'password': process.env.DB_PASS || 'password',
